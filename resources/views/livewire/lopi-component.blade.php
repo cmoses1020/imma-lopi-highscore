62afcbcded53ba9e9629f8712e893c89ddbf7dc1
Chris Moses <chris.moses@mosestechnologies.com>
2023-03-27 03:04:43 -0400

    wip

--- resources/views/livewire/lopi-component.blade.php
+++ resources/views/livewire/lopi-component.blade.php
@@ -1,24 +1,44 @@
 <div 
-    class="flex justify-center items-center text-center mt-40"
+    class="flex justify-center items-center text-center mt-12"
     x-data="lopi()"
     wire:poll.1000ms="poll"
 > 
     <div
-        class="fixed top-0 left-0 w-screen h-screen z-10 pointer-events-none"
+        class="flex justify-center items-center fixed top-0 left-0 w-screen h-screen z-10 pointer-events-none"
         x-ref="overlay"
         wire:ignore
-    ></div>
+    >
+        <div 
+            x-cloak
+            x-show="popup.show"
+            class="px-6 py-4 relative w-2/6 h-1/8 bg-lopi-purple-50 rounded-xl z-50"
+            x-transition:enter="transition ease-out duration-500"
+            x-transition:enter-start="opacity-0 transform scale-0"
+            x-transition:enter-end="opacity-100 transform scale-100"
+            x-transition:leave="transition ease-in duration-500"
+            x-transition:leave-start="opacity-100 transform scale-100"
+            x-transition:leave-end="opacity-0 transform scale-0"
+        >
+            <div class="absolute top-0 left-0 w-full h-2 bg-lopi-purple-500 rounded-tl-xl transition-all duration-75" :style="{ width: progressBar }"></div>
+            <div class="text-3xl text-left text-lopi-purple-500 rounded-lg">Warning!</div>
+            <p class="mt-2">The space key and enter key are disabled on this button.✌️</p>
+            {{-- progress bar until message vanishses --}}
+            <div class="absolute block bottom-0 left-0 w-full h-2 bg-lopi-purple-500 rounded-bl-xl transition-all duration-75" 
+                :style="{ width: progressBar }"
+            ></div>
+        </div>
+    </div>
     <div>
         <img class="mx-auto w-[200px]" src="{{ Vite::asset('resources/lopi_assets/lopibig.png') }}"></img>
         <h1 class="mt-[10px] mb-[5px] text-4xl font-bold">Enjoy some Lopi! <span class="text-[#561378] text-sm">(actually Jim)</span></h1>
         
-        <div wire:ingore x-text="client.userClicks" class="text-[#444] text-[24px] font-bold mt-[10px] font-['Trebuchet_MS'] relative z-50"></div>
+        <div wire:ingore x-text="client.userClicks" class="z-40 text-[#444] text-[24px] font-bold mt-[10px] font-['Trebuchet_MS'] relative"></div>
 
         <button
-            @keydown.enter.prevent="null"
-            @keydown.space.prevent="null"
+            @keydown.enter.prevent="showPopup"
+            @keydown.space.prevent="showPopup"
             x-on:click="click"
-            class="relative z-50 inline-block w-full text-[15px] bg-[#ffadd2] text-white font-bold py-[10px] px-[20px] border border-white hover:bg-opacity-80 hover:shadow-[#ffadd2] active:shadow-[#ffadd2] hover:shadow-md active:shadow-2xl active:bg-white active:text-[#ffadd2] active:border-[#ffadd2] shadow-md"
+            class="relative z-40 inline-block w-full text-[15px] bg-[#ffadd2] text-white font-bold py-[10px] px-[20px] border border-white hover:bg-opacity-80 hover:shadow-[#ffadd2] active:shadow-[#ffadd2] hover:shadow-md active:shadow-2xl active:bg-white active:text-[#ffadd2] active:border-[#ffadd2] shadow-md"
         >
             I'm Lopi
         </button>
@@ -76,10 +96,12 @@
     <script>
         let lopi = () => {
             return {
-                totalClicksInterval: null,
-                countInterval: null,
+                popup: {
+                    show: false,
+                    at: null,
+                },
+                progressBar: null,
                 countDelay: 1000 / 60,
-                pullDelay: 1000,
                 lastTime: 0,
                 client: {
                     userClicks: 0,
@@ -96,6 +118,18 @@
                 },
                 animate(timeStamp) {
                     this.animateCounts()
+
+                    // start timer when popup is shown and turn off after 5 seconds
+                    if (this.popup.show) {
+                        this.progressBar = `${100 - ((timeStamp - this.popup.at) / 50)}%`
+                        if (!this.popup.at) {
+                            this.popup.at = timeStamp
+                        } else if (timeStamp - this.popup.at > 5000) {
+                            this.popup.show = false
+                            this.popup.at = null
+                        }
+                    }
+
                     this.lopis.forEach((lopi) => {
                         lopi.animate(timeStamp)
                     })
@@ -111,6 +145,10 @@
                                 this.client[key] += Math.max(1, Math.floor((this.server[key] - this.client[key]) / 10))
                             }
                         })
+                },
+                showPopup() {
+                    this.popup.show = true
+                    this.popup.at = null
                 },
                 isGuest: @js(auth()),
                 lopis: [],
